baf9b84db3270f46bb2fd845d1ca47e3ffd5c018
UnKnown <97171985+Collon-D@users.noreply.github.com>
2023-10-15 10:28:54 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+#define ULL     unsigned long long
+#define DIVISOR 1234567891
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr), cout.tie(nullptr);
+
+    int N; cin >> N;
+    string tmp; cin >> tmp;
+    ULL answer = 0;
+
+    for (int i = 0; i < N; ++i) {
+        ULL multy = 1;
+        if (i == 0) 
+            answer += (tmp[i] - 'a' + 1) *  1;
+        else {
+            for (int j = 0; j < i; ++j) {
+                multy *= 31;
+            }
+            answer += (tmp[i] - 'a' + 1) * multy; 
+        }
+    }
+    answer %= DIVISOR;
+    cout << answer;
+
+    return 0;
+}
