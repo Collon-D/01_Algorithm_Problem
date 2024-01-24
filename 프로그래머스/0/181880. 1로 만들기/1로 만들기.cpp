1fb44252b5d9b58366437e362284cb5e1cc74dcd
UnKnown <97171985+Collon-D@users.noreply.github.com>
2024-01-24 13:09:05 +0900

    [level 0] Title: 1로 만들기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list) {
+    int answer = 0;
+    
+    for (int i = 0; i < num_list.size(); ++i) {
+        while (num_list[i] != 1) {
+            if (num_list[i] & 1) {
+                num_list[i] -= 1;
+                num_list[i] /= 2;
+            }
+            else {
+                num_list[i] /= 2;
+            }
+            answer++;
+        }
+    }
+    
+    return answer;
+}
