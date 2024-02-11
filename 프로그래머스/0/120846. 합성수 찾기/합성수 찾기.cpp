81c7428f0834d59565d8d36a78ae953274381b5c
UnKnown <97171985+Collon-D@users.noreply.github.com>
2024-02-11 16:19:07 +0900

    [level 0] Title: 합성수 찾기, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    
+    for (int i = 4; i <= n; ++i) {
+        for (int j = 2; j < i; ++j) {
+            if (i % j == 0) {
+                answer++;
+                break;
+            }
+        }
+    }
+    
+    return answer;
+}
