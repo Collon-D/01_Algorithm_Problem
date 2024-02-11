793a1d985703fdce06821530cda7f866115c10e1
UnKnown <97171985+Collon-D@users.noreply.github.com>
2024-02-11 16:58:00 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string my_string) {
+    int answer = 0;
+    
+    string tmp = "";
+    for (int i = 0; i < my_string.size(); ++i) {
+        if ('0' <= my_string[i] && my_string[i] <= '9') {
+            tmp += my_string[i];
+        }
+        else {
+            if (!tmp.empty()) {
+                answer += stoi(tmp);
+                tmp = "";    
+            }
+        }
+    }
+    
+    if (!tmp.empty()) {
+        answer += stoi(tmp);
+    }
+    
+    return answer;
+}
