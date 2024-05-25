ef002a02d25391be805ea2c8a77676ba232c91b2
UnKnown <97171985+Collon-D@users.noreply.github.com>
2024-05-25 13:22:11 +0900

    [Gold V] Title: 동전 1, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    int n, k; cin >> n >> k;
+    vector<int> coin(n);
+    vector<int> dp(k + 1);
+
+    for (int i = 0; i < n; ++i)
+        cin >> coin[i];
+
+    dp[0] = 1;
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = coin[i]; j <= k; ++j)
+        {
+            dp[j] += dp[j - coin[i]];
+        }
+    }
+
+    cout << dp[k];
+    return 0;
+}
